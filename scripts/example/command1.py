6f5ccfb1ff10fc4f2cce5ace9b6cb0eaaee883e5
Mateus Oliveira <matews1943@gmail.com>
2022-01-09 19:51:20 -0300

    fixup! hotfix: fix sonarcloud scan

--- scripts/example/command1.py
+++ scripts/example/command1.py
@@ -10,4 +10,10 @@ def command1(**kwargs):
         print(f'Number argument called with {kwargs["number"]}.')
 
 
-def error():
+# def command1(**kwargs):
+#     """Print example command."""
+#     print('Command 1 called.')
+#     if isinstance(kwargs.get('text'), str):
+#         print(f'Text argument called with {kwargs["text"]}.')
+#     if isinstance(kwargs.get('number'), int):
+#         print(f'Number argument called with {kwargs["number"]}.')
