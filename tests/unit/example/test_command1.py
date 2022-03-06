529792ca67bba555199053da2c40a9dea6882a3d
Mateus Oliveira <matews1943@gmail.com>
2022-03-06 11:42:40 -0300

    fixup! feat: add coverage reports to CI artifacts

--- tests/unit/example/test_command1.py
+++ tests/unit/example/test_command1.py
@@ -11,17 +11,17 @@ def test_command1_without_options(capsys):
     assert output == 'Command 1 called.\n'
 
 
-# def test_command1_with_text(capsys):
-#     """Test command1 with text."""
-#     command1(text='text')
-#     output, error = capsys.readouterr()
-#     assert not error
-#     assert 'Text argument called with text.\n' in output
+def test_command1_with_text(capsys):
+    """Test command1 with text."""
+    command1(text='text')
+    output, error = capsys.readouterr()
+    assert not error
+    assert 'Text argument called with text.\n' in output
 
 
-# def test_command1_with_number(capsys):
-#     """Test command1 with number."""
-#     command1(number=1)
-#     output, error = capsys.readouterr()
-#     assert not error
-#     assert 'Number argument called with 1.\n' in output
+def test_command1_with_number(capsys):
+    """Test command1 with number."""
+    command1(number=1)
+    output, error = capsys.readouterr()
+    assert not error
+    assert 'Number argument called with 1.\n' in output
