e0bf648972a47936e47fd67fa111d013775e02f0
Mateus Oliveira <matews1943@gmail.com>
2022-07-27 22:08:50 -0300

    fixup! fix: Add quality checks to Docker

--- docker/scan.sh
+++ docker/scan.sh
@@ -4,5 +4,5 @@ ROOT="$(dirname $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P ))"
 
 docker build --tag python-cli-script-template --file $ROOT/docker/Dockerfile .
 docker login
-docker scan --exclude-base --file $ROOT/docker/Dockerfile python-cli-script-template
+docker scan --severity medium --file $ROOT/docker/Dockerfile python-cli-script-template
 docker logout
