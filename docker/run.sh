e6187a113fe87a3f4f818b3622e58452c3a145ec
Luiz Filipe <moresco.luiz@gmail.com>
2023-04-17 21:38:05 +0200

    feat: Improve run.sh to use the correct DOCKER_COMPOSE binary (#77)

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,9 @@
 
 ROOT="$(dirname $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P ))"
 $ROOT/docker/env.sh
+. $ROOT/docker/base.sh
 
-docker-compose \
+$DOCKER_COMPOSE \
 --file $ROOT/docker/docker-compose.yaml \
 --project-directory $ROOT \
 run --rm cly $@
