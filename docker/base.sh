8cb4de1fbf1fd1fcb46e578892957445f1921826
Luiz Filipe <moresco.luiz@gmail.com>
2023-04-17 21:59:32 +0200

    feat: Improve base.sh to have 'docker compose' as a fallback (#77)

--- docker/base.sh
+++ docker/base.sh
@@ -4,9 +4,6 @@ DOCKER_COMPOSE=""
 
 if [ -x "$(command -v $DOCKER_COMPOSE_BIN)" ]; then
   DOCKER_COMPOSE=$DOCKER_COMPOSE_BIN
-elif [ -x "$(command -v $DOCKER_COMPOSE_FROM_DOCKER)" ]; then
-  DOCKER_COMPOSE=$DOCKER_COMPOSE_FROM_DOCKER
 else
-  echo "Neither 'docker-compose' and 'docker compose' found. At least one of them are mandatory."
-  exit 1
+  DOCKER_COMPOSE=$DOCKER_COMPOSE_FROM_DOCKER
 fi
