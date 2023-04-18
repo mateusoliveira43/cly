44e4c96edd18bb40e02234ea7d7a36d2817c4fa8
Luiz Filipe <moresco.luiz@gmail.com>
2023-04-18 19:59:57 +0200

    feat: Implement unit test to cover case insensitive ids (#84)

--- tests/unit/batcomputer_cli/commands/test_identify.py
+++ tests/unit/batcomputer_cli/commands/test_identify.py
@@ -1,9 +1,27 @@
+import string
 import pytest
 
 from ....batcomputer_cli.commands.identify import identify
 from ....batcomputer_cli.database import CHARACTERS
 
 NON_IDENTIFIED_CHARACTERS = ["riddler", "nightwing", "penguin"]
+
+CHARACTER = list(CHARACTERS)[0]
+CASE_INSENSITIVE_CHARACTERS = [
+    CHARACTER.lower(),
+    CHARACTER.upper(),
+    string.capwords(CHARACTER),
+]
+
+
+@pytest.mark.parametrize("alias", CASE_INSENSITIVE_CHARACTERS)
+def test_identify_with_case_insensitive_identified_alias(
+    alias: str, capsys: pytest.CaptureFixture[str]
+) -> None:
+    identify([alias])
+    output, error = capsys.readouterr()
+    assert not error
+    assert "A.K.A" in output
 
 
 @pytest.mark.parametrize("alias", CHARACTERS)
